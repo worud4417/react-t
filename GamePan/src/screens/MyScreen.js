98c185394bbdc1b92b61e0d0718217f9f83c9598
worud4417 <worud4417@gmail.com>
2019-08-21 01:00:07 +0900

    v.8.21-logintest

--- GamePan/src/screens/MyScreen.js
+++ GamePan/src/screens/MyScreen.js
@@ -1,7 +1,6 @@
 import React,{Component} from 'react';
 import {View,Text,Image,StyleSheet,TouchableOpacity,AsyncStorage} from 'react-native';
 
-import LoginComponent from '../components/LoginComponent';
 import { NavigationEvents } from 'react-navigation';
 
 export default class MyScreen extends Component{
@@ -30,50 +29,25 @@ export default class MyScreen extends Component{
         }
     }
 
-    _getUser= async ()=> {
-        try{
-//this will change to server connect---------------------------------------------
-            let name = await AsyncStorage.getItem('name')
-            let photo = await AsyncStorage.getItem('photo')
-//---------------------------------------------------------------------------------
-            this.setState({photo:photo,name:name})
-        }
-        catch{
-            await AsyncStorage.setItem('error','true')
-        }
-    }
-
-    _setLogin(){
-        this.setState({login:true})
-    }
-
     render(){
-        if(!this.state.login){
-            return(
-                <LoginComponent setLogin={this._setLogin.bind(this)}></LoginComponent>
-            )
-        }
-        else{
-            this._getUser()
-            return(
-                <View style={{flex:1}}>
-                    <View style={{flex:1,flexDirection:'row', borderBottomWidth:2}}>
-                        <View style={{flex:1}}>
-                            <Image source={{uri:this.state.photo}} style={styles.photo}></Image>
-                        </View>
-                        <View style={{flex:1}}>
-                            <Text style={{fontSize:30,margin:10,fontWeight:'bold'}}>{this.state.name}</Text>
-                            <Text>직업 : 조선민주주의인민공화국 수석 대변인</Text>
-                            <Text>특기 : 합법적 트롤</Text>
-                        </View>
+        return(
+            <View style={{flex:1}}>
+                 <View style={{flex:1,flexDirection:'row', borderBottomWidth:2}}>
+                    <View style={{flex:1}}>
+                        <Image source={{uri:this.state.photo}} style={styles.photo}></Image>
                     </View>
-                    <View style={styles.list}>
-                        <Text>거래내역</Text>
-                        <Text>내가 쓴글</Text>
+                    <View style={{flex:1}}>
+                        <Text style={{fontSize:30,margin:10,fontWeight:'bold'}}>{this.state.name}</Text>
+                        <Text>직업 : 조선민주주의인민공화국 수석 대변인</Text>
+                        <Text>특기 : 합법적 트롤</Text>
                     </View>
                 </View>
-            )
-        }
+                <View style={styles.list}>
+                    <Text>거래내역</Text>
+                    <Text>내가 쓴글</Text>
+                </View>
+            </View>
+        )
     }
 }
 
